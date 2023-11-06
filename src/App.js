594a238badafad75da11317f36dae4c76b7989b1
Lucas Regalado <lucasregalado@hotmail.com>
2023-11-06 19:25:40 -0300

    create container

--- src/App.js
+++ src/App.js
@@ -14,16 +14,18 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/SimpleFormPage" element={<SimpleFormPage />} />
-          <Route
-            path="/ConnectWithExternalApiPage"
-            element={<ConnectWithExternalApiPage />}
-          />
-          <Route path="/ReactKonvaPage" element={<ReactKonvaPage />} />
-          <Route path="*" element={<HomePage />} />
-        </Routes>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/SimpleFormPage" element={<SimpleFormPage />} />
+            <Route
+              path="/ConnectWithExternalApiPage"
+              element={<ConnectWithExternalApiPage />}
+            />
+            <Route path="/ReactKonvaPage" element={<ReactKonvaPage />} />
+            <Route path="*" element={<HomePage />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
