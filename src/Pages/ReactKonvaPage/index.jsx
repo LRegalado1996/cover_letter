9a6b22ec7bec79fb204987cd3b1e99eaa1e3eb79
Lucas Regalado <lucasregalado@hotmail.com>
2023-11-06 21:26:16 -0300

    create react conva page

--- src/Pages/ReactKonvaPage/index.jsx
+++ src/Pages/ReactKonvaPage/index.jsx
@@ -1,7 +1,18 @@
+import { BodyImage } from "../../Components";
+import "./style.scss";
+
 const ReactKonvaPage = () => {
   return (
     <div className="ReactKonvaPage">
-      <h1>ReactKonvaPage</h1>
+      <div className="containerIntroduction">
+        <h1>Image manipulation</h1>
+        <p>
+          Let's imagine that you are on a visit to the doctor. The doctor will
+          show you an image of the body and you must click on the parts of the
+          body that hurt.
+        </p>
+      </div>
+      <BodyImage />
     </div>
   );
 };
